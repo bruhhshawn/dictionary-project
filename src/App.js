383fcbc51248c54495fa8b535629e1a9d395c99e
bruhhshawn <saradf1996@gmail.com>
2022-09-18 07:57:05 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./dictionary-icon.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
