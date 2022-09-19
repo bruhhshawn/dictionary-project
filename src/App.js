0d10e61a29badf93b8b5cbb48f16feff6c9fe7b3
bruhhshawn <saradf1996@gmail.com>
2022-09-19 07:58:00 -0700

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from "./dictionary-icon.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
-      <p>Hello</p>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">
+          <small>Coded by Sara</small>
+        </footer>
+      </div>
     </div>
   );
 }
